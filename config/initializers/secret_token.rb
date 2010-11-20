109031a1c63d813a6ac9b30b321d1a7ff3f6e151
kayalemao <kayalemao@online.de>
2010-11-20 02:36:06 +0100

    Initial commit+

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandfyTest::Application.config.secret_token = '8cc9ee412a84a89642d8ec0cb84053fbba7197cbf199abb8aa19c56949fa4ba0b466895c5a9be6f2007bf930e60ba204b687cbf9b5be1cbca873de24ff17eb85'
